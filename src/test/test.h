a645322c8196b9459a81c8e48fa71b4cec5fdeee
Atila Ribeiro <atiladavip@gmail.com>
2026-09-06 02:11:36 -0300

    Adiciona arquivos de testes ausentes: src/test/test.h

---
+++ src/test/test.h
@@ -0,0 +1,9 @@
+// Antilua
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2026 Luanti Authors
+
+#pragma once
+
+#include "config.h"
+
+int run_catch2_tests(int argc, char *argv[]);
